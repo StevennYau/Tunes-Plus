0a2e6efa0a18d5c2e5462ecfe0e970aa55c1a4ba
StevennYau <stevenyau@gmail.com>
2021-01-20 15:59:37 -0500

    user top songs

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,21 +23,56 @@ const Home = ({ token }) => {
       
       
    }, []);
-   
+   //0 is user info
+   //1 is current song playing
+   //2 is top songs
+   //3 is top artists
    if (user){
-     console.log('user: ' + user[0]);
+     console.log('user0 : ' + user[0]);
          console.log(user[0]);
-         console.log('user2: ' + user[1]);
+         console.log('user1: ' + user[1]);
          console.log(user[1]);
+         console.log('user2:' + user[2]);
+         console.log(user[2]);
+         console.log('user3: ');
+         console.log(user[3])
       return (
          <div>
-            <h2>Welcome to Tunes Plus {user[0]["display_name"]}!</h2>
+            <h1>Welcome to Tunes Plus {user[0]["display_name"]}!</h1>
             <div>
-               <img className="userPic" src={user[0]["images"][0]["url"]}  alt="user profile picture"/>
+               <img className="userPic" src={user[0]["images"][0]["url"]}  alt="user pfp"/>
+               followers: {user[0].followers.total}
             </div>
+            
             <div>
-               <p>Current Song/ Most Recent Song: {user[1]["item"]["name"]}</p>
+               <h5>Current Song/ Most Recent Song:</h5>
+               {user[1]["item"] && 
+               <div>
+                  <h4>{user[1]["item"]["name"]}</h4>
+                  <img className="userPic" src={user[1]["item"].album.images[1].url}  alt="user current playing"/>
+               </div>
+               }
+               {typeof user[1]["item"] == "undefined" &&
+                  <div>Please open spotify and play a song </div>
+               }
+            </div>
 
+            <div>
+               <h5>Top Songs</h5>
+               <ol className="center">
+                    {user[2].map((song) =>(
+                        <li key={song}>{song}</li>
+                    ))}
+                </ol>
+            </div>
+
+            <div>
+               <h5>Top Artists</h5>
+               <ol className="center">
+                    {user[2].map((song) =>(
+                        <li key={song}>{song}</li>
+                    ))}
+                </ol>
             </div>
          </div>
       )
