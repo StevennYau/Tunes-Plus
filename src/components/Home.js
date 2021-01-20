c5ddf5b8baacb95c5f150f34910b2278295d37cf
StevennYau <stevenyau@gmail.com>
2021-01-20 16:45:51 -0500

    added user's top artists and their pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,6 +27,7 @@ const Home = ({ token }) => {
    //1 is current song playing
    //2 is top songs
    //3 is top artists
+   //4 is top artist images
    if (user){
      console.log('user0 : ' + user[0]);
          console.log(user[0]);
@@ -35,7 +36,15 @@ const Home = ({ token }) => {
          console.log('user2:' + user[2]);
          console.log(user[2]);
          console.log('user3: ');
-         console.log(user[3])
+         console.log(user[3]);
+      let topArtists = [];
+      for (let i = 0; i < user[2].length; i++){
+         <div>
+         topArtists.push(<li key={i}>{user[3][i]}</li>
+                         <img alt="test" src={user[4][i]}/>)
+         </div>
+      }
+      console.log('test: ' + topArtists[0]);
       return (
          <div>
             <h1>Welcome to Tunes Plus {user[0]["display_name"]}!</h1>
@@ -68,10 +77,47 @@ const Home = ({ token }) => {
 
             <div>
                <h5>Top Artists</h5>
-               <ol className="center">
-                    {user[2].map((song) =>(
-                        <li key={song}>{song}</li>
-                    ))}
+               <ol className="center form-inline">
+                    <div>
+                       <li key="0">{user[3][0]}</li>
+                       <img alt="artist1" src={user[4][0]}/>
+                    </div>
+                    <div>
+                    <li key="0">{user[3][1]}</li>
+                       <img alt="artist2" src={user[4][1]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][2]}</li>
+                       <img alt="artist3" src={user[4][2]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][3]}</li>
+                       <img alt="artist4" src={user[4][3]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][4]}</li>
+                       <img alt="artist5" src={user[4][4]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][5]}</li>
+                       <img alt="artist6" src={user[4][5]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][6]}</li>
+                       <img alt="artist7" src={user[4][6]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][7]}</li>
+                       <img alt="artist8" src={user[4][7]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][8]}</li>
+                       <img alt="artist9" src={user[4][8]}/>
+                    </div>
+                    <div>
+                       <li key="0">{user[3][9]}</li>
+                       <img alt="artist10" src={user[4][9]}/>
+                    </div>           
                 </ol>
             </div>
          </div>
@@ -84,7 +130,6 @@ const Home = ({ token }) => {
             </h1>
          </div>
       )
-   
    }
 }
 
