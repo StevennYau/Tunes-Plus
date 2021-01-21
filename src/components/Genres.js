2a07e4ce5f61ce28dab00b4182badc2e740efd0e
StevennYau <stevenyau@gmail.com>
2021-01-21 14:34:02 -0500

    added titles and pictures of genres

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 const Genres = ({ token }) => {
-    const [genres, setGenres] = useState([]);
+    const [genres, setGenres] = useState(null);
     const [clicked, setClicked] = useState(false);
     
-    const getGenre = () => {
+    /*const getGenre = () => {
         if (clicked === true) {
             setClicked(false);
             } else {
@@ -19,22 +19,56 @@ const Genres = ({ token }) => {
                 console.log("error is " + error);
                 return error;
             });
-    };
+    };*/
+    useEffect(() => {
+        axios.get("/getGenre", {withCredentials: true})
+            .then(response => {
+                setGenres(response.data)
+            })
+            .catch(error => {
+                console.log("error is " + error);
+                return error;
+            });
+    }, [])
 
-    return (
-        <div className="app">
-            <button onClick={getGenre}>Get genres (toggle)</button>
-            
-            {clicked === true && 
-                <ol className="center">
-                    {genres.map((item) =>(
-                        <li key={item}>{item}</li>
-                    ))}
-                </ol>
-                }
-        </div>
-    )
+
+    if (genres){
+        return (
+            <div className="app">
+            <div>
+              <h5>Genres</h5>
+             <ol className="center form-inline">
+                   {genres[0].map((name, index) =>(
+                      <div>
+                         <li className="genreText" key={genres.toString()}>{name}</li>
+                         <img className="genreImage" alt={name} src={genres[1][index]} />
+                      </div>
+                   ))}
+                   </ol>
+           </div>
+           {/*<button onClick={getGenre}>Get genres (toggle)</button>
+           
+           {clicked === true && 
+               <ol className="center">
+                   {genres.map((item) =>(
+                       <li key={item}>{item}</li>
+                   ))}
+               </ol>
+               }*/}
+       </div>
+        )
+     } else {
+        return(
+           <div>
+              <h1>
+                 Please Log In to View Spotify data!
+              </h1>
+           </div>
+        )
+     }
+  }
+   
     
-} 
+
 
   export default Genres;
